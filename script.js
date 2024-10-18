cd22b8a260af470466d95039029474fbc667e633
Advay Anand <advayanand37@gmail.com>
2024-10-18 19:41:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.querySelector ('.join-btn'). addEventListener('click', () => {
+    alert ("Wilol show the link");
+}
+);
 
+document.querySelector ('.explore-btn'). addEventListener ('click', () => {
+    alert ("Exploring the website!");
+})
+  ;
+
+document.querySelector ('.involve-btn'). addEventListener ('click', () => {
+    alert ("Get involved feature is getting in use .");
+})
+  ;
