5f9b81b82e5b9adc27b145df1875268ecb2f00f3
Advay Anand <advayanand37@gmail.com>
2024-10-20 00:19:55 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,13 +1,43 @@
 from django.http import JsonResponse
 import requests
+from twilio.rest import Client
 
-def home(request):
-    return JsonResponse({"message": " Running on Localhost"})
-
-def find_police_stations(request):
+def emergency_alert(request):
     lat = request.GET.get('lat')
     lon = request.GET.get('lon')
-    api_key = 'AIzaSyAUHGIIISpb2DdPZF57rZMemE1NEZfzbFc'
+    contacts = ['+6386771480']  # Replace with actual emergency contact numbers
+
+    for contact in contacts:
+        send_alert(contact, lat, lon)
+
+    return JsonResponse({'status': 'Alert sent successfully!'})
+
+def send_alert(contact_number, lat, lon):
+    account_sid = '#'
+    auth_token = '#'
+    client = Client(account_sid, auth_token)
+
+    try:
+        message = client.messages.create(
+            body=f'User in emergency. Location: {lat},{lon}',
+            from_='+15097743936',
+            to=contact_number
+        )
+        print(f"Alert sent to {contact_number}. Message SID: {message.sid}")
+    except Exception as e:
+        print(f"Failed to send alert: {e}")
+
+def find_nearby_places(request):
+    lat = request.GET.get('lat')
+    lon = request.GET.get('lon')
+    api_key = '#'
+
     endpoint = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lon}&radius=2000&type=police&key={api_key}'
-    response = requests.get(endpoint)
-    return JsonResponse(response.json())
+
+    try:
+        response = requests.get(endpoint)
+        response.raise_for_status()
+        data = response.json()
+        return JsonResponse(data)
+    except requests.exceptions.RequestException as e:
+        return JsonResponse({'error': str(e)}, status=400)
